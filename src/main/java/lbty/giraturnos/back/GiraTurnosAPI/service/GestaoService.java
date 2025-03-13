e3bad4c8fc5da87bc11b6a7e4919e773eeb3fe35
Gustavo Henrique <gustavosilvah37@gmail.com>
2025-03-12 22:27:51 -0300

    feat: gestao service and localizacao service finished

--- src/main/java/lbty/giraturnos/back/GiraTurnosAPI/service/GestaoService.java
+++ src/main/java/lbty/giraturnos/back/GiraTurnosAPI/service/GestaoService.java
@@ -1,4 +1,85 @@
 package lbty.giraturnos.back.GiraTurnosAPI.service;
 
+import lbty.giraturnos.back.GiraTurnosAPI.dto.GestaoDTO;
+import lbty.giraturnos.back.GiraTurnosAPI.entity.GestaoEntity;
+import lbty.giraturnos.back.GiraTurnosAPI.entity.TecnicoEntity;
+import lbty.giraturnos.back.GiraTurnosAPI.repository.GestaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
 public class GestaoService {
+
+    @Autowired
+    private GestaoRepository gestaoRepository;
+
+    public List<GestaoDTO> listAll(){
+        List<GestaoEntity> gestao = gestaoRepository.findAllOrderedByName();
+        return gestao.stream().map(GestaoDTO::new).toList();
+    }
+
+    public ResponseEntity<Map<String, String>> insertGestao(GestaoDTO gestaoDTO){
+        GestaoEntity gestaoEntity = new GestaoEntity(gestaoDTO);
+        Map<String, String> response = new HashMap<>();
+
+        try{
+            gestaoRepository.save(gestaoEntity);
+            response.put("WARN", "Gestao cadastrada com sucesso!!");
+            return ResponseEntity.ok(response);
+        }catch (RuntimeException re){
+            response.put("ERROR", "Erro ao cadastrar aluno: " + re.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
+
+    public ResponseEntity<Map<String, String>> updateGestao(GestaoDTO gestaoDTO, Long id){
+        Map<String, String> response = new HashMap<>();
+        Optional<GestaoEntity> gestaoEntityOptional = gestaoRepository.findById(id);
+
+        try{
+            if(gestaoEntityOptional.isPresent()){
+                GestaoEntity gestaoEntity = gestaoEntityOptional.get();
+                gestaoEntity.setNome(gestaoDTO.getNome());
+                gestaoEntity.setCargo(gestaoDTO.getCargo());
+                gestaoEntity.setEmail(gestaoDTO.getEmail());
+                gestaoEntity.setTelefone(gestaoDTO.getTelefone());
+                gestaoRepository.save(gestaoEntity);
+
+                response.put("WARN", "Gestao alterada com sucesso!");
+                return ResponseEntity.ok(response);
+            }else {
+                response.put("WARN", "Gestao não encontrada!");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+            }
+        }catch (RuntimeException re){
+            response.put("ERROR", "Erro ao alterar gestao: " + re.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
+
+    public ResponseEntity<Map<String, String>> deleteGestao(Long id){
+        GestaoEntity gestaoEntity = gestaoRepository.findById(id).get();
+        Map<String, String> response = new HashMap<>();
+
+        try{
+            if(gestaoEntity != null){
+                gestaoRepository.delete(gestaoEntity);
+                response.put("WARN", "Gestao removida com sucesso!!");
+                return ResponseEntity.ok(response);
+            }else {
+                response.put("WARN", "Gestao não encontrada!");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+            }
+        }catch (RuntimeException re){
+            response.put("ERROR", "Erro ao deletar gestao: " + re.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
 }
--- src/main/java/lbty/giraturnos/back/GiraTurnosAPI/service/LocalizacaoService.java
+++ src/main/java/lbty/giraturnos/back/GiraTurnosAPI/service/LocalizacaoService.java
@@ -1,4 +1,82 @@
 package lbty.giraturnos.back.GiraTurnosAPI.service;
 
+import lbty.giraturnos.back.GiraTurnosAPI.dto.LocalizacaoDTO;
+import lbty.giraturnos.back.GiraTurnosAPI.entity.LocalizacaoEntity;
+import lbty.giraturnos.back.GiraTurnosAPI.repository.LocalizacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
 public class LocalizacaoService {
+
+    @Autowired
+    private LocalizacaoRepository localizacaoRepository;
+
+    public List<LocalizacaoDTO> listAll(){
+        List<LocalizacaoEntity> localizacao = localizacaoRepository.findAllOrderedByName();
+        return localizacao.stream().map(LocalizacaoDTO::new).toList();
+    }
+
+    public ResponseEntity<Map<String, String>> insertLocalizacao(LocalizacaoDTO localizacaoDTO){
+        LocalizacaoEntity localizacaoEntity = new LocalizacaoEntity(localizacaoDTO);
+        Map<String, String> response = new HashMap<>();
+
+        try{
+            localizacaoRepository.save(localizacaoEntity);
+            response.put("WARN", "Localização cadastrada com sucesso!!");
+            return ResponseEntity.ok(response);
+        }catch (RuntimeException re){
+            response.put("ERROR", "Erro ao cadastrar localização: " + re.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
+
+    public ResponseEntity<Map<String, String>> updateLocalizacao(LocalizacaoDTO localizacaoDTO, Long id){
+        Map<String, String> response = new HashMap<>();
+        Optional<LocalizacaoEntity> localizacaoEntityOptional = localizacaoRepository.findById(id);
+
+        try{
+            if(localizacaoEntityOptional.isPresent()){
+                LocalizacaoEntity localizacaoEntity = localizacaoEntityOptional.get();
+                localizacaoEntity.setLocal(localizacaoDTO.getLocal());
+                localizacaoRepository.save(localizacaoEntity);
+
+                response.put("WARN", "Localização alterada com sucesso!");
+                return ResponseEntity.ok(response);
+            }else {
+                response.put("WARN", "Localização não encontrada!");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+            }
+        }catch (RuntimeException re){
+            response.put("ERROR", "Erro ao alterar localização: " + re.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
+
+    public ResponseEntity<Map<String, String>> deleteLocalizacao(Long id){
+        LocalizacaoEntity localizacaoEntity = localizacaoRepository.findById(id).get();
+        Map<String, String> response = new HashMap<>();
+
+        try{
+            if(localizacaoEntity != null){
+                localizacaoRepository.delete(localizacaoEntity);
+                response.put("WARN", "Localização removida com sucesso!!");
+                return ResponseEntity.ok(response);
+            }else {
+                response.put("WARN", "Localização não encontrada!");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+            }
+        }catch (RuntimeException re){
+            response.put("ERROR", "Erro ao deletar localização: " + re.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
+
 }
